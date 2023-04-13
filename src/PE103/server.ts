a7f2ca00f99d4f35696adddfdc652984be08016a
Diego <alu0101601830@ull.edu.es>
2023-04-13 10:18:10 +0000

    Ejercicios PE103

--- src/PE103/server.ts
+++ src/PE103/server.ts
@@ -21,29 +21,3 @@ import {watchFile} from 'fs';
   }).listen(60300, () => {
     console.log('Waiting for clients to connect.');
   });
-/*
-
-import net from 'net';
-import { exec } from 'child_process';
-
-net.createServer((connection) => {
-  console.log('A client has connected.');
-
-  connection.on('data', (data) => {
-    const { command, args } = JSON.parse(data.toString());
-    exec(`${command} ${args.join(' ')}`, (error, stdout, stderr) => {
-      if (error) {
-        connection.write(JSON.stringify({ type: 'error', message: error.message }) + '\n');
-      } else {
-        connection.write(JSON.stringify({ type: 'result', stdout, stderr }) + '\n');
-      }
-    });
-  });
-
-  connection.on('close', () => {
-    console.log('A client has disconnected.');
-  });
-}).listen(60300, () => {
-  console.log('Waiting for clients to connect.');
-});
-*/
